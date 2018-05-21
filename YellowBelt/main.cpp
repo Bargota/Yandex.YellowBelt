2ddb3e2b3471f5b62aaaddb7745eda58d4c5294d
0dyvan <v.orlov.geo@gmail.com>
2018-05-21 18:57:04 +0300

    add Regions 1week

--- YellowBelt/main.cpp
+++ YellowBelt/main.cpp
@@ -6,27 +6,38 @@
 #include "AvrTemperature.h"
 #include <iostream>
 #include "CubeWeight_1week.h"
+#include "region.h"
 
 int main()
 {
-	readFile r("cube.txt");
+	/*readFile r("cube.txt");
 	r.params_and_text();
 	std::vector<std::string> data;
 	r.getText(data);
 	std::vector<std::string> p;
-	r.getParams(p);
+	r.getParams(p);*/
 
 	//Temperature t;
 	//t.Run(data);
 
-	for (auto item : data)
+	/*for (auto item : data)
 	{
 		int i = 1;
 		Cube c(item, p[1]);
 		std::cout<<"weight cube numder "<<i<<" = " << c.getWeight() << std::endl;
 		i++;
+	}*/
+
+	RegionData r1;
+	std::pair<int, std::string>  p=FindMaxRepetitionCount(r1.reg2);
+	if (p.first != -1)
+	{
+		std::cout << p.second << " " << p.first << std::endl;
 	}
-	
+	else
+	{
+		std::cout << p.second << std::endl;
+	}
 	system("pause");
     return 0;
 }
--- YellowBelt/region.h
+++ YellowBelt/region.h
@@ -15,12 +15,19 @@ struct Region
 	std::map < Lang, std::string> names;
 	int64_t population;
 
+	
+
 };
 
-int FindMaxRepetitionCount(const std::vector<Region>& regions);
+bool operator==(Region lv,Region rv);
+bool operator==(std::tuple<std::string, std::string, std::map<Lang, std::string>, int64_t> lv, std::tuple<std::string, std::string, std::map<Lang, std::string>, int64_t> rv);
+//bool operator==(std::map<Lang, std::string> lv, std::map<Lang, std::string> rv);
+
+std::pair<int,std::string> FindMaxRepetitionCount(const std::vector<Region>& regions);
 
 class RegionData
 {
+public:
 	const std::vector<Region> reg1 = { {
 			"Moscow",
 			"Russia",
@@ -47,4 +54,32 @@ class RegionData
 			{ { Lang::DE, "Russland" },{ Lang::FR, "Russie" },{ Lang::IT, "Russia" } },
 			89
 		} };
+	const std::vector<Region> reg2 = {
+		{
+			"Moscow",
+			"Russia",
+			{ { Lang::DE, "Moskau" },{ Lang::FR, "Moscou" },{ Lang::IT, "Mosca" } },
+			89
+		},{
+			"Russia",
+			"Eurasia",
+			{ { Lang::DE, "Russland" },{ Lang::FR, "Russie" },{ Lang::IT, "Russia" } },
+			89
+		},{
+			"Moscow",
+			"Russia",
+			{ { Lang::DE, "Moskau" },{ Lang::FR, "Moscou deux" },{ Lang::IT, "Mosca" } },
+			89
+		},{
+			"Moscow",
+			"Toulouse",
+			{ { Lang::DE, "Moskau" },{ Lang::FR, "Moscou" },{ Lang::IT, "Mosca" } },
+			89
+		},{
+			"Moscow",
+			"Russia",
+			{ { Lang::DE, "Moskau" },{ Lang::FR, "Moscou" },{ Lang::IT, "Mosca" } },
+			31
+		},
+	};
 };
